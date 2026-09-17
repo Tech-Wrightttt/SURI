9bf3171f74f4f2ba3523fde73b7f854255383888
Lovely Shane Ong <134838637+xienshane@users.noreply.github.com>
2026-09-17 15:44:51 +0800

    feat: new login ui

--- frontend/app/session/[session_id]/diagnostic/page.tsx
+++ frontend/app/session/[session_id]/diagnostic/page.tsx
@@ -7,6 +7,7 @@ import confetti from "canvas-confetti";
 import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
 import {
   getSession,
